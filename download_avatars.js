f5a1683cc7626236420b5707f83803a376936275
nhoss050 <nima.hjavaheri@gmail.com>
2017-02-14 03:11:16 +0000

    user required to enter repoowner/name

--- download_avatars.js
+++ download_avatars.js
@@ -1,30 +1,39 @@
 var request = require ('request');
 var fs = require('fs')
 console.log('Welcome to the GitHub Avatar Downloader!');
+var userinput = process.argv.slice(2);
+  var OwnerOfRepo = userinput[0]
+  var nameOfRepo = userinput[1]
 
 function getRepoContributors(repoOwner, repoName, cb) {
 
   var GITHUB_USER = "nhoss050";
   var GITHUB_TOKEN = "061e2c2d30fd19d4aaa022458d0abc5f4f985fa8";
   var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-// console.log(requestURL);
 
-  request.get({
-    url:requestURL, headers:{'User-Agent':'rohit'},
-  }, function(err, response, body) {
-    if (err) cb(null, err); // throw err;
-    cb(JSON.parse(response.body));
-  });
 
-  // ...
-  // request.get(requestURL, function(err, response, body) {
-  //   if (err) throw err;
-  //   console.log('Response Status Code:', response.body);
-  // });
+  //console.log(typeof repoName);
+
+//
+    if(OwnerOfRepo){
+      if(nameOfRepo){
+        request.get({
+        url:requestURL, headers:{'User-Agent':'rohit'},
+        }, function(err, response, body) {
+             if (err) cb(null, err); // throw err;
+             cb(JSON.parse(response.body));
+        });
+      }else {
+      console.log('name of the Repo is required!')
+      }
+
+    } else {
+      console.log('Owner of the Repo is required!')
+    }
 
 }
 
-getRepoContributors("jquery", "jquery", function(result, err) {
+getRepoContributors(OwnerOfRepo, nameOfRepo, function(result, err) {
   //console.log("Errors: ", err);
   // console.log("Result: \n", result);
 
